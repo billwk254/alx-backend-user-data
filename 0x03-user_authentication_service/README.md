0193b93ffe75b0811900b6190aa01cca1661e6b0
billwk254 <billkihungu51@gmail.com>
2024-02-22 17:23:06 +0300

    a SQLAlchemy model named User for a database table named users

---
+++ 0x03-user_authentication_service/README.md
@@ -0,0 +1,13 @@
+# User Authentication System
+
+This repository contains a Python script for a simple user authentication system. It includes a User class defined using SQLAlchemy for database interaction.
+
+## Contents
+
+- `user_authentication.py`: Defines the `User` class for user authentication purposes.
+- `requirements.txt`: Lists the dependencies required to run the script.
+
+## Usage
+
+1. Clone the repository:
+
---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+User Authentication System: Defines the User class for authentication purposes.
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+# Base class for ORM
+Base = declarative_base()
+
+class User(Base):
+    """
+    User class representing registered users in the authentication system.
+    """
+    __tablename__ = 'users'
+
+    id = Column(Integer, autoincrement="auto", primary_key=True)
+    email = Column(String(250), nullable=False)  # User's email address
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))  # Session ID for logged-in users
+    reset_token = Column(String(250))  # Token used for password reset
